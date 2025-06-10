0ce22cb3b907884deb18d1bcd4551a03cb1ffaf5
mark-aique <mark.cabalona@aiqueinnovation.com>
2025-06-10 09:01:44 +0800

    use WebsocketChannel instead if IOWebsocketChannel if platform is web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2564610752"
+    serviceWorkerVersion: "1276685208"
   }
 });
